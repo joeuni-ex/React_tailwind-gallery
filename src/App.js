27dda889c4d567a2392efe3c0c1743b062e0aecc
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-20 12:46:31 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("flowers"); //검색어
 
   //사진 가져옴
