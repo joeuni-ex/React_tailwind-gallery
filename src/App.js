8cdf54c2280d9018f290ac2c6f76c9ba7af7bc66
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-20 10:14:33 +0900

    픽사베이API키 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import "./App.css";
-function App() {
+function useStateApp() {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
@@ -19,11 +18,16 @@ function App() {
           </li>
         </ul>
       </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+      <div className="px-6 py-4 text-sm font-semibold text-gray-700">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  mr-2">
           #tag1
         </span>
-        ? ?
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  mr-2">
+          #tag3
+        </span>
       </div>
     </div>
   );
